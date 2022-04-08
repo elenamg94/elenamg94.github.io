a1f9117e2a175b5c9d45efe7d979f7ce3e2e3d17
Elena Martínez González <96417328+elenamg94@users.noreply.github.com>
2022-04-08 06:54:26 +0000

    añadi js

---
+++ javascript/js-ejerc03-operadores-binarios.js
@@ -0,0 +1,13 @@
+var A = 10;
+var B = 20;
+var divide =A/B;
+var suma = A+B;
+A==B
+A!=B
+A<B
+document.write("<p>El resultado de dividir ",A, " entre ",B, " es ",divide,"</p>")
+document.write("<p>El resultado de sumar ",A," más ",B, " es ",suma,"</p>")
+document.write("<p>El resultado de evaluar si ",A, " y ",B, " son iguales es: ",A==B,"</p>")
+document.write("<p>El resultado de evaluar si ",A, " y ",B, " son distintos es: ",A!=B,"</p>")
+document.write("<p>El resultado de evaluar si ",A, " es menor que ",B, " es: ",A<B,"</p>")
+document.write("<p>El resultado de evaluar si ",A, " es mayor que ",B, " es: ",A>B,"</p>")
